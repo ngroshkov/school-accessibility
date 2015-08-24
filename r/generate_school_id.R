c6d50108ea281d98aa0c972bbd328dc23dca4b1d
Nikolay Groshkov <ngroshkov@yandex.ru>
2015-08-24 09:06:29 +0300

    verify schools assightd area

--- r/generate_school_id.R
+++ r/generate_school_id.R
@@ -32,3 +32,32 @@ building2school.new <-function(schools, school2building){
   };
   building2school.new;
 }
+
+assigned.area.best.school.ids <- function(schools.without.assigned.area, schools.new) {
+  assigned.area.best.school.ids <- data.frame();
+  
+  for (sIdx in 1:nrow(schools.without.assigned.area)) {
+    id <- schools.without.assigned.area$NEW_ID[sIdx];
+    complexId <- schools.without.assigned.area$COMPLEX_ID[sIdx];
+    
+    ss <- subset(schools.new, COMPLEX_ID == complexId);
+    ss <- ss[order(ss$HOUSE_ID_COUNT, decreasing = TRUE),];
+    ss <- ss[c(1, 12)];
+    
+    ss$SCHOOL_ID <- id; 
+    ss$ASSIGNED_SCHOOL_ID <- ss$NEW_ID;
+    ss <- ss[c(3,4,2)];
+    
+#   ss <- ss[ss$SCHOOL_ID != ss$ASSIGNED_SCHOOL_ID,];
+    
+    ss$SELF <- ss$SCHOOL_ID == ss$ASSIGNED_SCHOOL_ID;
+    ss$ZERO_ASSIGNED <- nrow(ss) >= 1 & ss$HOUSE_ID_COUNT[1] == 0;
+    
+    
+    assigned.area.best.school.ids <- rbind(assigned.area.best.school.ids, ss[1,]);
+#   assigned.area.best.school.ids <- rbind(assigned.area.best.school.ids, ss);
+
+    
+  };
+  assigned.area.best.school.ids;
+}
