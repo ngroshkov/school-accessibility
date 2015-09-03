adfb75ba5243d1e5bcbd18dc8cb5de77e199580a
Nikolay Groshkov <ngroshkov@yandex.ru>
2015-09-03 13:18:43 +0300

    fix school2building

--- r/generate_school_id.R
+++ r/generate_school_id.R
@@ -12,6 +12,20 @@ building2school.generate.new.school.id = function(schools, school2building){
   building2school.generate.new.school.id <- school2building;
 }
 
+building2school.generate.new.building.id = function(buildings, school2building){
+  
+  for (bIdx in 1:nrow(buildings)) {
+    
+    bId <- buildings$ID[bIdx];
+    bNewId <- buildings$NEW_ID[bIdx];
+    
+    school2building$NEW_BUILDING_ID[school2building$NEW_BUILDING_ID == bId] <- bNewId;
+    print(paste(bIdx, " ", buildings$NAME[bIdx], " ", round((100*bIdx)/nrow(buildings), digits = 2), "%", sep = ""));
+  };
+  
+  building2school.generate.new.building.id <- school2building;
+}
+
 building2school.new <-function(schools, school2building){
   building2school.new <- data.frame();
   
@@ -60,4 +74,43 @@ assigned.area.best.school.ids <- function(schools.without.assigned.area, schools
     
   };
   assigned.area.best.school.ids;
+}
+
+calc.distance.between.buildings.double <- function(buildings.double, buildings) {
+  calc.distance.between.buildings.double <- numeric();
+  
+  for (bIdx in 1:nrow(buildings.double)) {
+    id <- buildings.double$ID[bIdx];
+    
+    builds <- buildings[buildings$ID == id,]
+    
+    p1 <- c(builds$POINT_X[1], builds$POINT_Y[1]);
+    p2 <- c(builds$POINT_X[2], builds$POINT_Y[2])
+
+    distance <- distVincentyEllipsoid(p1, p2);
+    
+    calc.distance.between.buildings.double <- c(calc.distance.between.buildings.double, distance)
+    
+    print(paste(bIdx, " ", round((100*bIdx)/nrow(buildings.double), digits = 2), "%", sep = ""));
+  };
+  
+  calc.distance.between.buildings.double;
+}
+
+calc.same.students <- function(buildings.double, buildings) {
+  calc.same.students <- logical();
+  
+  for (bIdx in 1:nrow(buildings.double)) {
+    id <- buildings.double$ID[bIdx];
+    
+    builds <- buildings[buildings$ID == id,]
+    s1 <- builds$STUDENTS[1];
+    s2 <- builds$STUDENTS[2];
+
+    calc.same.students <- c(calc.same.students, s1 == s2)
+    
+    print(paste(bIdx, " ", round((100*bIdx)/nrow(buildings.double), digits = 2), "%", sep = ""));
+  };
+  
+  calc.same.students;
 }
