706d5e1a91c728673da6ed92b3a134d950527762
ngroshkov <ngroshkov@yandex.ru>
2017-01-10 10:46:12 +0300

    count for buildings accessible schools with rating

--- r/choise.R
+++ r/choise.R
@@ -26,14 +26,17 @@ freqhist.choise  <- function(buildings, field, show.titles = TRUE) {
   }
   
   freqhist.choise <- ggplot(buildings, aes_string(x=field, weight = "STUDENTS")) +
-    geom_histogram(fill=colors$COLOR, binwidth = 1) +
+  #  geom_histogram(fill=colors$COLOR, binwidth = 1) +
+    geom_bar(fill=colors$COLOR, width = 1) +
   #  scale_x_discrete(name=x.axis.name, expand = c(0,2), limits=c(0:14), labels=c("","","","","","","","","","","","","","","")) +
   #  scale_y_continuous(name=y.axis.name, breaks=seq(0, 325, by = 100), limits=c(0, 340), labels=c("","","","")) +
   #  stat_bin(binwidth=1, geom="text", aes(label=round(..count..)), size=3.5, colour="#4C4646", fontface="bold", vjust=-0.5) +
     scale_x_discrete(name=x.axis.name, expand = c(0,2), limits=c(0:14)) +
     scale_y_continuous(name=y.axis.name, breaks=seq(0, 325, by = 100), limits=c(0, 340)) +
-    stat_bin(binwidth=1, geom="text", aes(label=round(..count..)), size=3.5, colour="#4C4646", fontface="bold", vjust=-0.5) +
-    
+   # stat_bin(binwidth=1, geom="text", aes(label=round(..count..)), size=3.5, colour="#4C4646", fontface="bold", vjust=-0.5) +
+  #  geom_text(aes(y=..count.., label=..count..), stat="count", color="white", hjust=1.0, size=3) +
+  #  geom_text(binwidth=1, geom="text", aes(label=round(..count..)), size=3.5, colour="#4C4646", fontface="bold", vjust=-0.5) +
+    geom_text(stat="count", aes(label=round(..count..)), size=3.5, colour="#4C4646", fontface="bold", vjust=-0.5) +
     theme(legend.position="none") +
     theme_minimal() +
     ggtitle(title);
